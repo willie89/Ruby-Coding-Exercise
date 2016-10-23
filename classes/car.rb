565fdf6f5bbd17d5da51c3bfd47774d98c18ecd2
John Chan <johntcchan@gmail.com>
2016-10-22 18:12:00 -0700

    Added getter and setter methods

--- classes/car.rb
+++ classes/car.rb
@@ -1,37 +1,67 @@
 class Car
 
-  def move_forward
-    puts "VROOM VROOM"
+  def initialize
+    @cost = 4000
+    @top_speed= 60
+    @max_fuel = 40
   end
 
-  def turn
-    puts "SCREECH"
+  def cost
+    @cost
   end
 
-  def brake
-    puts "I stopped."
+  def cost=(value)
+    @cost = value
   end
 
-  def self.turn
-    puts "Cars can turn, yes."
+  def top_speed
+    @top_speed
   end
 
-  def who_am_i?
-    self
+  def top_speed=(value)
+    @top_speed = value
   end
 
-  class << self
-
-    def stuff
-      puts "stuff"
-    end
-
-    def whatevs
-      puts "whatevs"
-    end
-
-    def who_am_i?
-      self
-    end
+  def max_fuel
+    @max_fuel = value
   end
+
+  def max_fuel=(value)
+    @max_fuel = value
+  end
+
+  # def move_forward
+  #   puts "VROOM VROOM"
+  # end
+
+  # def turn
+  #   puts "SCREECH"
+  # end
+
+  # def brake
+  #   puts "I stopped."
+  # end
+
+  # def who_am_i?
+  #   self
+  # end
+
+  # def self.turn
+  #   puts "Cars can turn, yes."
+  # end
+
+  # class << self
+
+  #   def stuff
+  #     puts "stuff"
+  #   end
+
+  #   def whatevs
+  #     puts "whatevs"
+  #   end
+
+  #   def who_am_i?
+  #     self
+  #   end
+  # end
 end
